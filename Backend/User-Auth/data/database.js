54b5a1afb94319ff9c460ddde61d62588ab77610
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-01-29 11:02:50 +0530

    added backend setup with express, mongodb, and user authentication

--- Backend/User-Auth/data/database.js
+++ Backend/User-Auth/data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect("mongodb+srv://rijanspatoliyacg:event123@cluster0.ms4no.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { dbName: "EventTracker" })
+    .connect("mongodb+srv://rijanspatoliyacg:event123@cluster0.ms4no.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { dbName: "EventTracker"})
     .then(() => console.log("Database Connected"))
     .catch((e) => console.log("Error in connecting to database: ", e));
 };
