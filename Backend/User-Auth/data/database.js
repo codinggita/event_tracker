1b29510f40f634fb6e8aac2e16ae716ed98e5f7e
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-02-04 14:28:48 +0530

    URI changes

--- Backend/User-Auth/data/database.js
+++ Backend/User-Auth/data/database.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const connectDB = () => {
   mongoose
-    .connect("mongodb+srv://rijanspatoliyacg:event123@cluster0.ms4no.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { dbName: "EventTracker"})
+    .connect(process.env.MONGO_URI, { dbName: "EventTracker" })
     .then(() => console.log("Database Connected"))
     .catch((e) => console.log("Error in connecting to database: ", e));
 };
