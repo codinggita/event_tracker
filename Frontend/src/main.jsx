64fd72897444945dac3b2f86a837bc8a2bd78f1f
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-02-06 01:10:03 +0530

    Updated layout and main components, added styling in index.css, and updated package.json

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
 export const server = "https://eventtracker-user.onrender.com/api/v1/users"
 
 createRoot(document.getElementById('root')).render(
