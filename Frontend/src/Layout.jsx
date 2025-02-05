3f78bb46208a233132355fadf20a88c95c7151af
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-02-06 01:11:17 +0530

    updated layout file

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,29 +1,41 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import Nav from "./Component/Nav";
 import Home from "./Component/Home";
 import Login from "./Component/Login";
 import SignUp from "./Component/SignUp";
 import {Toaster} from "react-hot-toast";
+import Trending from "./Pages/TrendingPage";
+import Upcoming from "./Pages/UpcomingPage";
+import Festival from "./Pages/FestivalPage";
+import Business from "./Pages/BusinessPage";
+import OnlyNavbar from "./Component/OnlyNav";
 
 
 function Layout() {
   const location = useLocation();
 
-  const hideNav = location.pathname === "/Login" || location.pathname === "/SignUp";
+  const hideNav =
+    location.pathname === "/Login" ||
+    location.pathname === "/SignUp" ||
+    location.pathname === "/";
 
   return (
     <>
-      {!hideNav && <Nav />}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
+      {!hideNav && <OnlyNavbar />}
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/upcoming" element={<Upcoming />} />
+          <Route path="/festival" element={<Festival />} />
+          <Route path="/business" element={<Business />} />
+        </Routes>
+      
       <Toaster />
     </>
   );
 }
 
-export default Layout;
+export default Layout
