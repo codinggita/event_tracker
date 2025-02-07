d0e7139542f0d4cacfbc94ba2b380b06bd4d447d
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-02-07 11:03:38 +0530

    update cokkies

--- Backend/User-Auth/app.js
+++ Backend/User-Auth/app.js
@@ -10,11 +10,20 @@ dotenv.config({ path: path.resolve('data', 'config.env') });
 const app = express(); // Initialize Express
 
 // Enable CORS
-app.use(cors());
-connectDB(); // Connect to the database
+app.use(cors({
+  origin: 'http://localhost:5173', // Your frontend URL (during development)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow necessary HTTP methods
+  credentials: true,  // Allow credentials such as cookies or authentication headers
+}));
+
+// Middleware
+app.use(cookieParser());  // Parse cookies if any are sent
+app.use(express.json());   // Parse incoming JSON requests
 
 // Using Routes
 app.use("/api/v1/users", userRouter);
+
+connectDB(); // Connect to the database
 
 // Start the server
 app.listen(process.env.PORT, () => {
