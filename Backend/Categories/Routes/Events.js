6bc894aa66aae9b6cb4bb31796c20957f43e622f
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-02-21 16:34:45 +0530

    Refactored event retrieval logic

--- Backend/Categories/Routes/Events.js
+++ Backend/Categories/Routes/Events.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { createEvent, allEvents, yourEvents, editEvent, deleteEvent } from "../Controllers/Events.js";
+import { createEvent, allEvents, yourEvents, editEvent, deleteEvent, eventDetailPage } from "../Controllers/Events.js";
 import { checkAuth } from "../middleware/auth.js";  // ✅ Middleware Import
 
 const router = express.Router();
 
 router.post("/createEvent", checkAuth, createEvent); // ✅ Only Authenticated Users
-router.get("/allEvents", allEvents); // ✅ Public Access
+router.get("/allEvents",checkAuth,allEvents); // ✅ Public Access
 router.get("/yourEvents", checkAuth, yourEvents);
 router.put("/:id", checkAuth, editEvent);
 router.delete("/:id", checkAuth, deleteEvent);
+router.get("/eventDetail/:id",checkAuth,eventDetailPage)
 
 export default router;
