7d3bd6116cc1d1cb624807a728ed2bf791379866
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-02-27 00:34:05 +0530

    Implement category-based event fetching in controller,Add category-based events route and Update event model

--- Backend/Categories/Routes/Events.js
+++ Backend/Categories/Routes/Events.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createEvent, allEvents, yourEvents, editEvent, deleteEvent, eventDetailPage,searchEvents  } from "../Controllers/Events.js";
+import { createEvent, allEvents, yourEvents, editEvent, deleteEvent, eventDetailPage,searchEvents, categoryEvents } from "../Controllers/Events.js";
 import { registerForEvent, getRegisteredUsers } from "../Controllers/RegistrationController.js";
 import { checkAuth } from "../middleware/auth.js";  // ✅ Middleware Import
 
 const router = express.Router();
 
 router.post("/createEvent", checkAuth, createEvent); // ✅ Only Authenticated Users
-router.get("/allEvents",checkAuth,allEvents); // ✅ Public Access
+router.get("/allEvents",allEvents); // ✅ Public Access
 router.get("/yourEvents", checkAuth, yourEvents);
 router.put("/editEvent/:id", checkAuth, editEvent);
 router.delete("/deleteEvent/:id", checkAuth, deleteEvent);
@@ -14,5 +14,7 @@ router.get("/eventDetail/:id",checkAuth,eventDetailPage);
 router.post("/registerEvent", registerForEvent);
 router.get("/registeredUsers/:eventId", getRegisteredUsers);
 router.get("/searchEvents", searchEvents); // ✅ Search API
+router.get("/category/:category", categoryEvents);
+
 
 export default router;
