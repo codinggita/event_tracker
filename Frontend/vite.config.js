d475248b461f03be3ec81348aca55c6c23988371
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-04-19 09:59:25 +0530

    fixed deployment issue

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,12 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: './', // 👈 ADD THIS LINE
   plugins: [react()],
   server: {
-    port: 5173, 
+    port: 5173,
   },
   resolve: {
     alias: {
