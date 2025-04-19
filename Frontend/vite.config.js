7e10ce921d2ba4165a893e1280a64855448569b2
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2025-04-19 10:24:16 +0530

    fixed issue

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,17 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";  // Add this import
 
 export default defineConfig({
-  base: './', // 👈 ADD THIS LINE
+  base: './',
   plugins: [react()],
   server: {
     port: 5173,
   },
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),  // Add this line
       "@chakra-ui/react": "@chakra-ui/react/dist/index.mjs",
     },
   },
